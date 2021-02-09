38130bd2976e6d889d021e4d6c817b215c716d50
Abraham <isolisrmz@gmail.com>
2021-02-08 22:43:51 -0600

    Se agrego la pantalla para ver un pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisesService } from '../../services/paises.service';
+import { Pais } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styles: [
+    `
+    .flag{
+      width: 100px;
+      heigth: 60px;
+    }
+    `
   ]
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor(private activatedRoute: ActivatedRoute ,private paisService: PaisesService) { }
+  pais!: Pais
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisesService, 
+    ) { }
 
   ngOnInit(): void {
     this.verPais();
   }
 
   verPais(){
-    this.activatedRoute.params.subscribe( ({id}) => {
-      this.paisService.verPais(id).subscribe( data => {
-        console.log(data);
-      });
+    this.activatedRoute.params
+      .pipe( switchMap ((params) => this.paisService.verPais(params.id)),
+      tap( console.log ))
+      .subscribe(resp => {
+        this.pais = resp;
     });
   }
 
